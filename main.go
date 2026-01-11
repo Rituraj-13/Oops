3f51813c8c3ebf1ed0c2794e21ad6e811f8f52e1
Rituraj Dey <riturajdey01@gmail.com>
2026-01-11 23:12:29 +0530

    added promptLine func

--- main.go
+++ main.go
@@ -2,26 +2,43 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/charmbracelet/lipgloss"
 )
 
+type config struct {
+	name   string `json:"name"`
+	APIKey string `json:apiKey`
+}
+
 func main() {
 
-	// var ApiKey string
+	style := lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("#FAFAFA")).
+		Width(44)
 
-	var style = lipgloss.NewStyle().
-    Bold(true).
-    Foreground(lipgloss.Color("#FAFAFA")).
-    Width(44)
-
-	// fmt.Println(style.Render("Hello, kitty"))
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print(style.Render("Please enter your Gemini API key: "))
-	line, _ := reader.ReadString('\n')
-
-	fmt.Printf("Your API Key is : %s", line)
-	
 }
+
+// This function will be used to show the message and take the inptut from the terminal
+
+func PromptLine(reader *bufio.Reader, style lipgloss.Style, label string)(string, error){
+	fmt.Println(style.Render(label))
+	s, err := reader.ReadString('\n')
+	if err != nil && !errors.Is(err, io.EOF){
+		return "", nil
+	}
+
+	s = strings.TrimSpace(s)
+	if s == "" {
+		return "", errors.New("Value cant be empty")
+	}
+	return s, nil
+}
+``
+func config
